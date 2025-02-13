b6bdd4391e4d7dad76903a1cf79e45d3c5b90380
Obieda11 <obiedamansour@gmail.com>
2025-02-13 18:42:19 +0000

    Fix: Variable fixed

--- script.py
+++ script.py
@@ -1,4 +1,13 @@
 print("Welcome to Obieda's Best Cinema World!")
-print(input("How Old Are You? "))
+age = int(input("How Old Are You? "))
 Bill = 0 
 
+if age <= 12:
+    print("your ticket price is 5$ ")
+    Bill + 5
+elif age >12 and age <18:
+    print("your ticket price is 8$ ")  
+else:
+    print("your ticket price is 10$")
+
+print("Thank you for your visit")
