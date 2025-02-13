7bb49f1d9948897664a1d17c22d3824ff43098dd
Obieda11 <obiedamansour@gmail.com>
2025-02-13 19:08:29 +0000

    Feat: added If/else

--- Excercise2.py
+++ Excercise2.py
@@ -1,2 +1,7 @@
 print("Hello, and welcome to the game of your life! ")
 Number = int(input("Give me a Number!: "))
+
+if Number % 2 == 0:
+    print("your number is even")
+else:
+    print("your number is Odd")
