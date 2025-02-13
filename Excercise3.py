543ae93f2d3ae3e96c08c856d5fa8b2e9f141650
Obieda11 <obiedamansour@gmail.com>
2025-02-13 19:25:00 +0000

    Feat: added line below welcoming

--- Excercise3.py
+++ Excercise3.py
@@ -1,4 +1,5 @@
 print("Welcome to the secret Application!")
+print("Please log in using your details, ensuring that no CAPITAL letters are used!")
 Login = input("Username: ")
 Pass = input("Password: ")
 
